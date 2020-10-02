df03b028b6c9879e16df98746a8ece5b0ebce88d
Chris Sainty <chris@chrissainty.com>
2020-10-02 08:33:53 +0100

    First draft of chapter 3

---
+++ chapter-03/BlazingTrails/BlazingTrails.Web/Features/Shared/Trail.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace BlazingTrails.Web.Features.Shared
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Location { get; set; }
+        public string Time { get; set; }
+        public int Length { get; set; }
+        public bool IsFavourite { get; set; }
+        public IEnumerable<RouteInstruction> Route { get; set; }
+    }
+
+    public class RouteInstruction
+    {
+        public int Stage { get; set; }
+        public string Description { get; set; }
+    }
+}
