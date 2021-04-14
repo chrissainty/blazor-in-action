0d9338d7da12bc66cb1d8752f2753ca60bacc468
Chris Sainty <chris@chrissainty.com>
2021-04-14 06:13:13 +0100

    Chapter 7 - WIP

---
+++ chapter-07/BlazingTrails/BlazingTrails.Web/Features/ManageTrails/EditTrail/EditTrailHandler.cs
@@ -0,0 +1,33 @@
+﻿using BlazingTrails.Shared.Features.ManageTrails.EditTrail;
+using MediatR;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazingTrails.Web.Features.ManageTrails.EditTrail
+{
+    public class EditTrailHandler : IRequestHandler<EditTrailRequest, EditTrailRequest.Response>
+    {
+        private readonly HttpClient _httpClient;
+
+        public EditTrailHandler(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<EditTrailRequest.Response> Handle(EditTrailRequest request, CancellationToken cancellationToken)
+        {
+            var response = await _httpClient.PutAsJsonAsync(EditTrailRequest.RouteTemplate, request, cancellationToken);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return new EditTrailRequest.Response(true);
+            }
+            else
+            {
+                return new EditTrailRequest.Response(false);
+            }
+        }
+    }
+}
