705387375ce674fdb47d8d7afb4f630be0be2fb1
Chris Sainty <chrissainty@users.noreply.github.com>
2021-04-19 06:48:15 +0100

    Chapter 07 (#5)

---
+++ chapter-07/BlazingTrails/BlazingTrails.Api/Startup.cs
@@ -0,0 +1,65 @@
+using BlazingTrails.Api.Persistance;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Hosting;
+using System.IO;
+
+namespace BlazingTrails.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddRazorPages();
+            services.AddDbContext<BlazingTrailsContext>(options => options.UseSqlite(Configuration.GetConnectionString("BlazingTrailsContext")));
+            services.AddDatabaseDeveloperPageExceptionFilter();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions()
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Images")),
+                RequestPath = new Microsoft.AspNetCore.Http.PathString("/Images")
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+                endpoints.MapFallbackToFile("index.html");
+            });
+        }
+    }
+}
---
+++ chapter-07/BlazingTrails/BlazingTrails.Web/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using MediatR;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BlazingTrails.Web
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMediatR(typeof(Program).Assembly);
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
