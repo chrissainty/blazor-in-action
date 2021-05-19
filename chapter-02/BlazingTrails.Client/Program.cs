4b39a69403f3fb205f3dfb82ec06e06d64c35c75
Chris Sainty <chrissainty@users.noreply.github.com>
2021-05-19 08:13:54 +0100

    Chapter 2 updates

--- chapter-02/BlazingTrails.Client/Program.cs
+++ chapter-02/BlazingTrails.Client/Program.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace BlazingTrails.Web
+namespace BlazingTrails.Client
 {
     public class Program
     {
