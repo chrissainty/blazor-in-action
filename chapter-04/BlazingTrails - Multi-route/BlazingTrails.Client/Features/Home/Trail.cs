a8c2a08e91521bc77fe0c820d993c8def953302e
Chris Sainty <chrissainty@users.noreply.github.com>
2021-05-21 20:02:33 +0100

    Chapter 4 updates

---
+++ chapter-04/BlazingTrails - Multi-route/BlazingTrails.Client/Features/Home/Trail.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace BlazingTrails.Client.Features.Home
+{
+    public class Trail
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Location { get; set; }
+        public int TimeInMinutes { get; set; }
+        public string TimeFormatted => $"{TimeInMinutes / 60}h {TimeInMinutes % 60}m";
+        public int Length { get; set; }
+        public IEnumerable<RouteInstruction> Route { get; set; }
+    }
+
+    public class RouteInstruction
+    {
+        public int Stage { get; set; }
+        public string Description { get; set; }
+    }
+}
