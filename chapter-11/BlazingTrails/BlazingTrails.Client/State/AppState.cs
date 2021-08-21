72578094e24520aae1630c942de8c65d85494ec1
Chris Sainty <chris@chrissainty.com>
2021-08-21 12:19:33 +0100

    Initial commit

---
+++ chapter-11/BlazingTrails/BlazingTrails.Client/State/AppState.cs
@@ -0,0 +1,28 @@
+﻿using Blazored.LocalStorage;
+using System.Threading.Tasks;
+
+namespace BlazingTrails.Client.State
+{
+    public class AppState
+    {
+        private bool _isInitialized;
+
+        public AddTrailState AddTrailState { get; }
+        public FavoriteTrailsState FavoriteTrailsState { get; }
+
+        public AppState(ILocalStorageService localStorageService)
+        {
+            AddTrailState = new AddTrailState();
+            FavoriteTrailsState = new FavoriteTrailsState(localStorageService);
+        }
+
+        public async Task Initialize()
+        {
+            if (!_isInitialized)
+            {
+                await FavoriteTrailsState.Initialize();
+                _isInitialized = true;
+            }
+        }
+    }
+}
