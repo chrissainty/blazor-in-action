19304cc297044b4f0af6c193ef8b4f71e5741a03
Chris Sainty <chris@chrissainty.com>
2021-09-02 17:03:38 +0100

    wip

--- chapter-11/BlazingTrails/BlazingTrails.Tests/Client/Features/Home/GetTrailsHandler.cs
+++ chapter-11/BlazingTrails/BlazingTrails.Tests/Client/Features/Home/GetTrailsHandler.cs
@@ -1,6 +1,6 @@
-﻿using BlazingTrails.Shared.Features.Home.Shared;
+﻿using AutoFixture;
+using BlazingTrails.Shared.Features.Home.Shared;
 using MediatR;
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -10,48 +10,10 @@ namespace BlazingTrails.Tests.Client.Features.Home
     {
         public async Task<GetTrailsRequest.Response> Handle(GetTrailsRequest request, CancellationToken cancellationToken)
         {
-            return new GetTrailsRequest.Response(new List<GetTrailsRequest.Trail>
-            {
-                new GetTrailsRequest.Trail(
-                    1,
-                    "Test Trail 1",
-                    "testtrail1.png",
-                    "Test Location",
-                    60,
-                    5,
-                    "Test Description",
-                    "Test User",
-                    new List<GetTrailsRequest.Waypoint>
-                    {
-                        new GetTrailsRequest.Waypoint(1.55m, 2.33m)
-                    }),
-                new GetTrailsRequest.Trail(
-                    2,
-                    "Test Trail 2",
-                    "testtrail1.png",
-                    "Test Location",
-                    60,
-                    5,
-                    "Test Description",
-                    "Test User",
-                    new List<GetTrailsRequest.Waypoint>
-                    {
-                        new GetTrailsRequest.Waypoint(1.55m, 2.33m)
-                    }),
-                new GetTrailsRequest.Trail(
-                    3,
-                    "Test Trail 3",
-                    "testtrail1.png",
-                    "Test Location",
-                    60,
-                    5,
-                    "Test Description",
-                    "Test User",
-                    new List<GetTrailsRequest.Waypoint>
-                    {
-                        new GetTrailsRequest.Waypoint(1.55m, 2.33m)
-                    })
-            });
+            var fixture = new Fixture();
+            var dummyTrails = fixture.CreateMany<GetTrailsRequest.Trail>();
+
+            return new GetTrailsRequest.Response(dummyTrails);
         }
     }
 }
